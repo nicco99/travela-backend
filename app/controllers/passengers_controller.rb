fccfa467ea5a051f21cf38af7be8eb6285c46601
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-12-02 19:43:19 +0300

    TRAV-8 updates

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,42 @@
 class PassengersController < ApplicationController
+# rescue_from ActiveRecord::RecordNotFound, with: :not_found
+# rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+# def create
+# user = User.create(passenger_params)
+# render json: user, status: :created
+# end
+
+# def show
+# user = User.find_user
+# render json: user, status: :ok
+# end
+
+# def delete
+# user = User.find_user
+# user.destroy
+# head :no_content
+# end
+
+# def update
+# user = User.find_user
+# user.update(passenger_params)
+# render json: user, status: :ok
+# end
+
+# private
+
+# def passenger_params
+# params.permit(:username, :password,:email,:p_number,:password,:confirm_password)
+# end
+# def find_user
+# user = User.find(params[:id])
+# end
+
+# def not_found
+# render json: {error: "Passenger not found"}, status: :not_found
+# end
+# def invalid_record invalid
+# render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
+# end
+
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,3 @@
 class Passenger < ApplicationRecord
+    validates :username,:email,:p_number,:password_digest, presence: true
 end
