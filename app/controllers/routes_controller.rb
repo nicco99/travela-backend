41204a178643ce700b54b2e957fb0b4147244aeb
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-12-04 09:14:23 +0300

    Routes actions init

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,2 +1,7 @@
 class RoutesController < ApplicationController
+
+    def index
+        routes = Route.all
+        render json: routes
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :reviews
-  resources :trips
+  resources :trips, only: [:index]
   resources :buses
   resources :routes
   resources :bookings
